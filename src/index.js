ee4d7b7a25815e4781768d82dedbd439ebc0e5b5
Rodrigo Mota <rodrigo.mota@trsolucoes.com>
2020-06-04 18:02:26 -0300

    api code update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import _ from 'lodash';
 import player from 'play-sound';
 import config from './config.js';
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 // read the configurations
 let {
   apiKey, apiSecret, amount, amountCurrency, initialBuy, minProfitPercent, intervalSeconds, playSound, simulation,
