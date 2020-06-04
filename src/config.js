e6e44b1425933e9019adf4ab7ad8695ceeb2a4d5
Rodrigo Mota <rodrigo.mota@trsolucoes.com>
2020-06-04 17:50:45 -0300

    api code update

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ import _ from 'lodash';
 
 
 const config = {
-  apiKey: null,
-  apiSecret: null,
-  amount: 100,
+  apiKey: '1042101d554993f0eadfa095a088dfa821fef8915c8f875ee74883c6e735d2b3',
+  apiSecret: '31b2f54a55aa79a122d4966abf974a1dd4a6981cc6efcbbbc86ba2d061772aa3',
+  amount: 2000,
   amountCurrency: 'BRL',
   initialBuy: true,
   minProfitPercent: 0.02,
