5ebd939b44dda8dcc825000f599a6e98ad631b74
Rodrigo Mota <rodrigo.mota@trsolucoes.com>
2020-06-04 18:07:13 -0300

    api code update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import _ from 'lodash';
 import player from 'play-sound';
 import config from './config.js';
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-
 // read the configurations
 let {
   apiKey, apiSecret, amount, amountCurrency, initialBuy, minProfitPercent, intervalSeconds, playSound, simulation,
@@ -249,4 +243,4 @@ async function start() {
   await startTrading();
 }
 
-start().catch(e => handleMessage(JSON.stringify(e), 'error'));
+start().listen(process.env.PORT, '0.0.0.0').catch(e => handleMessage(JSON.stringify(e), 'error'));
